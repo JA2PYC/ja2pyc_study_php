9f496dfed331acdea86582485ea676e1ecca5b6c
gr2nade <gr2nade@gmail.com>
2024-10-24 21:42:08 +0900

    Set php.ini and timezone

---
+++ ch01/processorder.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Order Results</title>
+</head>
+
+<body>
+    <h1>Bob's Auto Parts</h1>
+    <h2>Order Results</h2>
+    <?php
+    echo '<p>Order processed.</p>';
+    /*
+    Multiple Line Comment
+    */
+    // Line Comment Type (//)
+    # Line Comment Type (#)
+
+    echo "<p> Order processed at " . date('H:i, jS F Y') . "</p>";
+    // Comment ?> <p>Not Comment</p>
+</body>
+
+</html>
